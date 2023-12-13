35d4995779748e4668c441ea38e91fc58dd16a83
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2023-12-13 13:57:40 +0000

    Add Button for moving items to complete

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -40,5 +40,17 @@ def get_items():
             trello_card['status'] = trello_list['name']
             items.append(trello_card)
 
-
     return items
+
+def move_item_to_done(item_id):
+    reqUrl = f"https://api.trello.com/1/cards/{item_id}"
+
+    query_params={
+        "key": os.getenv("TRELLO_API_KEY"),
+        "token": os.getenv("TRELLO_API_TOKEN"),
+        "idList": os.getenv("TRELLO_DONE_LIST_ID")
+    }
+
+    response = requests.put(reqUrl, params = query_params)
+
+    response.raise_for_status()
