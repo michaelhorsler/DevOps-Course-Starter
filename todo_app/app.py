1bf24ddd1dd299d9506f2d09bd3e83f5988e564b
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2023-12-13 11:22:03 +0000

    Move creation of items to Trello

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, redirect, render_template, request
-from todo_app.data.session_items import add_item, get_items
+from todo_app.data.session_items import get_items
+from todo_app.data.trello_items import add_item
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
-
 
 @app.route('/')
 def index():
