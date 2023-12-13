6f1065150b429bb340f494ccd80c661f11376623
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2023-12-13 14:16:05 +0000

    Add item class

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,6 +2,8 @@ import requests
 import dotenv
 import os
 
+from todo_app.data.item import Item
+
 def add_item(title):
     reqUrl = "https://api.trello.com/1/cards"
 
@@ -37,8 +39,9 @@ def get_items():
 
     for trello_list in response_list:
         for trello_card in trello_list['cards']:
-            trello_card['status'] = trello_list['name']
-            items.append(trello_card)
+            item = Item.from_trello_card(trello_card, trello_list)
+  #          trello_card['status'] = trello_list['name']
+            items.append(item)
 
     return items
 
