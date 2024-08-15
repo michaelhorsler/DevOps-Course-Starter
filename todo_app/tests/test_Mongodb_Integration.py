751d2ef1637f918562babd4b26fcccaab721a0f9
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2024-08-15 17:39:11 +0100

    Add Mongodb Integration test

---
+++ todo_app/tests/test_Mongodb_Integration.py
@@ -0,0 +1,14 @@
+import mongomock
+import pytest
+from todo_app import app
+from dotenv import load_dotenv, find_dotenv
+
+@pytest.fixture
+def client():
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+    
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
