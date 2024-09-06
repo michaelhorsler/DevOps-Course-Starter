b9f2f93ed5cf32b2453c41cc020990800c48e0e8
Michael Horsler <Michael.Horsler@boschrexroth.co.uk>
2024-09-06 09:44:50 +0100

    Add monkeypatch

--- todo_app/tests/test_Mongodb_Integration.py
+++ todo_app/tests/test_Mongodb_Integration.py
@@ -6,12 +6,16 @@ from todo_app import app
 from dotenv import load_dotenv, find_dotenv
 from todo_app.data.mongo_items import get_post_collection, get_items
 from flask_dance.consumer.storage import MemoryStorage
+from todo_app.oauth import blueprint
 
 @pytest.fixture
-def client():
+def client(monkeypatch):
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
     
+    storage = MemoryStorage({"access_token": "fake-token"})
+    monkeypatch.setattr(blueprint, 'storage', storage)
+
     with mongomock.patch(servers=(('fakemongo.com', 27017),)):
         test_app = app.create_app()
         with test_app.test_client() as client:
